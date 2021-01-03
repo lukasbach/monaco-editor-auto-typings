2c0750d222164e293d2352f3c18fcbd78e4fafd4
Lukas Bach <lbach@outlook.de>
2021-01-03 22:10:37 +0100

    Updated demo code

--- packages/demo/src/editorContents.ts
+++ packages/demo/src/editorContents.ts
@@ -1,8 +1,15 @@
-export const editorContents = `import * as monaco from 'monaco-editor'
-import { BigIntEntityInterface, UncomplexEntityInterface } from "uncomplex";
-import React from 'react';
+export const editorContents = `import fetch from 'node-fetch'
+import React from 'react'
+import * as math from 'mathjs'
 
-const x = BigIntEntityInterface;
-const y: UncomplexEntityInterface = {
-  entityId: 'asd'
-}`;
+// Works fine
+fetch('https://google.com', { method: 'GET' })
+React.useEffect(() => {})
+React.useState<string>('Hello')
+math.derivative('x^2 + x', 'x')
+
+// Type errors are detected! :)
+fetch(1337) // Shouldn't be a number!
+React.useEffect('I\`m not a function!')
+React.useState<number>('Not a number :s')
+math.derivative('x^2 + x') // Error, argument missing!`;
