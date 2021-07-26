387e5c4e3e9471eb6ef3884e8ba18eda34683931
Lukas Bach <lbach@outlook.de>
2021-07-26 19:52:10 +0200

    Update monaco versions

--- packages/demo/src/editorContents.ts
+++ packages/demo/src/editorContents.ts
@@ -1,15 +1,15 @@
 export const editorContents = `import fetch from 'node-fetch'
 import React from 'react'
-import * as math from 'mathjs'
+import { LocalStorageCache } from 'monaco-editor-auto-typings'
 
 // Works fine
 fetch('https://google.com', { method: 'GET' })
 React.useEffect(() => {})
 React.useState<string>('Hello')
-math.derivative('x^2 + x', 'x')
+new LocalStorageCache().getFile('FILE_ID')
 
 // Type errors are detected! :)
 fetch(1337) // Shouldn't be a number!
 React.useEffect('I\`m not a function!')
 React.useState<number>('Not a number :s')
-math.derivative('x^2 + x') // Error, argument missing!`;
+new LocalStorageCache().getFile() // Argument missing!`;
