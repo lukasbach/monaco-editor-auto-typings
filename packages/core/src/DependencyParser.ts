399bfffc70c46aa8d39a0765e729c96c71b8f0eb
Lukas Bach <lbach@outlook.de>
2022-06-19 23:27:58 +0200

    support resolving node:x imports (#14)

--- packages/core/src/DependencyParser.ts
+++ packages/core/src/DependencyParser.ts
@@ -1,22 +1,21 @@
 import { ImportResourcePath } from './ImportResourcePath';
 import * as path from 'path';
-
 export class DependencyParser {
   private REGEX_CLEAN = /[\n|\r]/g;
   private REGEX_DETECT_IMPORT = /(?:(?:(?:import)|(?:export))(?:.)*?from\s+["']([^"']+)["'])|(?:require(?:\s+)?\(["']([^"']+)["']\))|(?:\/+\s+<reference\s+path=["']([^"']+)["']\s+\/>)/g;
+  private REGEX_NODE_MODULE = /^node:([\w\W\/]+)$/;
 
   public parseDependencies(source: string, parent: ImportResourcePath | string): ImportResourcePath[] {
     const cleaned = source; // source.replace(this.REGEX_CLEAN, '');
     return [...cleaned.matchAll(this.REGEX_DETECT_IMPORT)]
       .map(x => x[1] ?? x[2] ?? x[3])
       .filter(x => !!x)
-      .map(imp => {
-        const result = this.resolvePath(imp, parent);
-        return result;
-      });
+      .map(imp => this.resolvePath(imp, parent));
   }
 
   private resolvePath(importPath: string, parent: ImportResourcePath | string): ImportResourcePath {
+    const nodeImport = importPath.match(this.REGEX_NODE_MODULE);
+
     if (typeof parent === 'string') {
       if (importPath.startsWith('.')) {
         return {
@@ -30,6 +29,13 @@ export class DependencyParser {
           kind: 'package',
           packageName: `${segments[0]}/${segments[1]}`,
           importPath: segments.slice(2).join('/'),
+        };
+      } else if (nodeImport) {
+        return {
+          kind: 'relative-in-package',
+          packageName: '@types/node',
+          importPath: `${nodeImport[1]}.d.ts`,
+          sourcePath: ''
         };
       } else {
         const segments = importPath.split('/');
