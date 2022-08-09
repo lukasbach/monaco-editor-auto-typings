54d694925b38e8adaa4b695cd0de0fc71494cbc8
Lukas Bach <lbach@outlook.de>
2022-08-09 22:52:25 +0200

    fix: workaround for localstorage failures (#17)

--- packages/core/src/LocalStorageCache.ts
+++ packages/core/src/LocalStorageCache.ts
@@ -2,16 +2,27 @@ import { SourceCache } from './SourceCache';
 
 export class LocalStorageCache implements SourceCache {
   public static LOCALSTORAGE_PREFIX = '__autotyper_cache_';
+  private localCache: Record<string, string> = {};
 
   public async getFile(uri: string): Promise<string | undefined> {
-    return localStorage.getItem(LocalStorageCache.LOCALSTORAGE_PREFIX + uri) ?? undefined;
+    try {
+      return localStorage.getItem(LocalStorageCache.LOCALSTORAGE_PREFIX + uri) ?? undefined;
+    } catch (e) {
+      return this.localCache[uri];
+    }
   }
 
   public async storeFile(uri: string, content: string): Promise<void> {
-    localStorage.setItem(LocalStorageCache.LOCALSTORAGE_PREFIX + uri, content);
+    this.localCache[uri] = content;
+    try {
+      localStorage.setItem(LocalStorageCache.LOCALSTORAGE_PREFIX + uri, content);
+    } catch (e) {
+      // Ignore
+    }
   }
 
   public async clear(): Promise<void> {
+    this.localCache = {};
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
       if (key?.startsWith(LocalStorageCache.LOCALSTORAGE_PREFIX)) {
